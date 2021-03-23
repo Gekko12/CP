1408cb0fd36cf41459aa07350df16aad4c38e10b
Gekko12 <gauravsharma01042000@gmail.com>
2021-03-23 13:56:09 +0530

    CP startup code

---
+++ CP_startup.cpp
@@ -0,0 +1,32 @@
+/*
+To increase the startup speed during solving competitive programming 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define endl "\n"
+#define mod 1000000007
+#define int long long int
+
+void solve()
+{
+	//logical part starts from here
+}
+
+int32_t main(void)
+{
+	fast_io;
+	
+	int t;	//test-cases
+	cin>>t;
+	
+	while(t--)
+	{
+		//iterating part test cases
+		solve();
+	}
+	
+	return 0;
+}
